0e61a4cd5443dd0ce05452986b985eb020ad9972
artemihx <117065473+artemihx@users.noreply.github.com>
2024-04-17 15:02:08 +0700

    Start project

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class App
+{
+
+    class Calculate
+    {
+        public double a;
+        public double b;
+        public string op;
+    }
+
+    static Calculate InputMath()
+    {
+        Calculate math = new Calculate();
+        Console.WriteLine("Введите первое число");
+        math.a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите знак");
+        math.op = Console.ReadLine();
+        Console.WriteLine("Введите второе число");
+        math.b = Convert.ToDouble(Console.ReadLine());
+        return math;
+    }
+
+    static void Main()
+    {
+        Calculate math = InputMath();
+        Console.WriteLine($"{math.a}, {math.op}, {math.b}");
+    }
+}
